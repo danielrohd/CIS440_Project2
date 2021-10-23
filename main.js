b8755a24ad2e937d06d926ebe5adce9095aca391
Daniel Rohd <drohd@asu.edu>
2021-10-23 16:08:20 -0700

    added new module 'ejs' and added account info to welcome screen as test

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ const bodyParser = require("body-parser");
 const encoder = bodyParser.urlencoded();
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 //app.use(express.json());
 app.use("/home.css",express.static("home.css"));
+
+let userAccount;
 
 const connection = mysql.createConnection({
     host: "107.180.1.16",
@@ -29,7 +32,8 @@ app.post("/",encoder,function(req,res){
     var password = req.body.password;
     connection.query("select * from UserAccounts where username= ? and password= ?;",[username,password],function(error,results,fields){
         if(results.length >0){
-            res.redirect("/welcome");
+            userAccount = new User(results[0].username, results[0].password, results[0].firstName, results[0].lastName, results[0].email, results[0].birthday)
+            res.redirect("/views/welcome");
         }
         else{
             res.redirect("/");
@@ -40,8 +44,12 @@ app.post("/",encoder,function(req,res){
 })
 
 //when login is success
-app.get("/welcome",function(req,res){
-    res.sendFile(__dirname + "/welcome.html")
+app.get("/views/welcome",function(req,res){
+    // res.sendFile(__dirname + "/welcome.html")
+    // res.send(`<p>${userAccount.first} ${userAccount.last}<br>${userAccount.username}<br>${userAccount.birthdayString}<br>${userAccount.email}</p>`);
+    res.render('welcome', {
+        userAccount: userAccount
+    })
 })
 
 app.listen(3000);
@@ -53,7 +61,11 @@ class User {
         this.first = first;
         this.last = last;
         this.email = email;
-        this.birthday = birthday;
+        this.birthday = [];
+        birthday[0] = birthday.getFullYear();
+        birthday[1] = birthday.getMonth()+1;
+        birthday[2] = birthday.getDate();
+        this.birthdayString = `${birthday[1]}/${birthday[2]}/${birthday[0]}`
         this.orgList = [];
     }
 }
