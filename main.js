cb14491b4f6075e99822e036cc9f2169849b7593
Daniel Rohd <drohd@asu.edu>
2021-11-01 15:11:24 -0700

    commented out some issues

--- main.js
+++ main.js
@@ -323,27 +323,27 @@ class Step {
     }
 }
 
-//new stuff for the homepage things
-function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
+// //new stuff for the homepage things
+// function openPage(pageName, elmnt, color) {
+//     // Hide all elements with class="tabcontent" by default */
+//     var i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//       tabcontent[i].style.display = "none";
+//     }
   
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
-    }
+//     // Remove the background color of all tablinks/buttons
+//     tablinks = document.getElementsByClassName("tablink");
+//     for (i = 0; i < tablinks.length; i++) {
+//       tablinks[i].style.backgroundColor = "";
+//     }
   
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
+//     // Show the specific tab content
+//     document.getElementById(pageName).style.display = "block";
   
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
-  }
+//     // Add the specific color to the button used to open the tab content
+//     elmnt.style.backgroundColor = color;
+//   }
   
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+//   // Get the element with id="defaultOpen" and click on it
+//   document.getElementById("defaultOpen").click();
